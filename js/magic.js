4ed33dd8aa496e88fd27a2784b53b6606c62b421
Ramona McBrearty <ramona.mcbrearty@gmail.com>
2025-09-05 13:48:26 -0400

    implemented random answer generation

--- js/magic.js
+++ js/magic.js
@@ -1,1 +1,30 @@
-console.log("Script loaded!");
+const ball = document.getElementById("8-ball-img");
+const answer = document.getElementById("magic-answer");
+
+// array of possible answers
+const answersArr = [
+    "yes!",
+    "no.",
+    "perhaps...",
+    "ask again later.",
+    "the answer is unclear...",
+    "hell no!",
+    "don't count on it",
+    "wait and see."
+];
+
+// function to get & display answer
+function getAnswer(){
+
+    const min = 0;
+    const max = answersArr.length - 1;
+
+    // random int function to get index of answer
+    const randIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+    answer.innerHTML = answersArr[randIndex];
+}
+
+// click event listener to get answer
+ball.addEventListener("click", function(){
+    getAnswer();
+});
